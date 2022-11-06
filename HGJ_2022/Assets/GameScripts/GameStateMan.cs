edfe6a1b9d9c70e1793e852505ec9ae3c1097e21
HoodedBale <chewweiqiang@gmail.com>
2022-11-06 17:31:39 +0800

    animation

--- HGJ_2022/Assets/GameScripts/GameStateMan.cs
+++ HGJ_2022/Assets/GameScripts/GameStateMan.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameStateMan : MonoBehaviour
 {
     public GameObject lorry;
+    public WaveManager wavemanager;
     bool triggerLose = false;
+    bool triggerWin = false;
+
+    public Text waveNumberTxt;
+    public Text waveCountdownTxt;
 
     // Start is called before the first frame update
     void Start()
@@ -22,11 +28,23 @@ public class GameStateMan : MonoBehaviour
             triggerLose = true;
             StartCoroutine(LoseRoutine());
         }
+
+        if(!triggerWin && wavemanager.currentWaveMaster >= 6)
+        {
+            triggerWin = true;
+            StartCoroutine(Winroutine());
+        }
     }
 
     IEnumerator LoseRoutine()
     {
         yield return new WaitForSeconds(3);
         SceneManager.LoadScene("LoseScreen");
+    }
+
+    IEnumerator Winroutine()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("WinScreen");
     }
 }
